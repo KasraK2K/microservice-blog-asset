6c833a974259bb2d2f4b2b36bbf476d28ab3f5d5
Kasra Karami <Kasra_K2K@yahoo.com>
2023-01-14 10:26:31 +0330

    Force it to work with fastify #3
    Co-Authored-By: Kasra Karami <30779339+KasraK2K@users.noreply.github.com>
    Co-Authored-By: Kaveh Karami <48356643+KavehKarami@users.noreply.github.com>

---
+++ src/main.ts
@@ -0,0 +1,17 @@
+import { NestFactory } from '@nestjs/core'
+import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify'
+import { Logger } from '@nestjs/common'
+import { AppModule } from './app.module'
+
+async function bootstrap() {
+  const logger = new Logger(bootstrap.name)
+
+  const port = process.env.PORT || 3000
+  const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter())
+
+  await app
+    .listen(port, '0.0.0.0')
+    .then(() => logger.log(`Asset service is running on port ${port}`))
+    .catch((error) => logger.error(error))
+}
+bootstrap()
