e11945513554011d73dfc830142dc59e8cb0983a
Kasra Karami <Kasra_K2K@yahoo.com>
2023-01-27 10:29:47 +0330

    Create config file #1
    Create env config with four separated env file. (default, development, development local, production)
    
    Co-Authored-By: Kaveh Karami <48356643+KavehKarami@users.noreply.github.com>
    Co-Authored-By: kasrakaramikk <122204823+kasrakaramikk@users.noreply.github.com>

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,19 @@
-import { Module } from '@nestjs/common';
+import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath:
+        process.env.NODE_ENV === 'development'
+          ? ['.env.development', '.env']
+          : process.env.NODE_ENV === 'production'
+          ? ['.env.production', '.env']
+          : ['.env.development.local', '.env'],
+      isGlobal: true
+    })
+  ],
   controllers: [],
-  providers: [],
+  providers: []
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { AppModule } from './app.module'
 async function bootstrap() {
   const logger = new Logger(bootstrap.name)
 
-  const port = process.env.PORT || 3000
+  const port = process.env.PORT || 3003
   const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter())
 
   await app
